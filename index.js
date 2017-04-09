c30f15d1dc26d36ea71b2a7d5b405280a5acffab
林丰 <linhui@linfengdeMacBook-Air.local>
2017-04-09 22:24:37 +0800

    初步实现了两个效果, 不要在意 vue 文件里面写的乱, 这个玩意反正是你自己替换用的

---
+++ index.js
@@ -0,0 +1,34 @@
+import Vue from 'vue'
+import SpinnerComponent from './SpinnerWithMaskComponent'
+import merge from './utils/merge'
+
+let instance;
+
+let SpinnerConstructor = Vue.extend(SpinnerComponent);
+
+let initInstance = () => {
+	instance = new SpinnerConstructor({
+		el: document.createElement('div')
+	});
+	document.body.appendChild(instance.$el);
+}
+
+let Spinner = {
+	start (content, options={}) {
+		if (!instance) {
+			initInstance();
+		}
+		options.content = content;
+		merge(instance.$data, options);
+		instance.showSpinner = true;
+	},
+	stop () {
+		instance.showSpinner = false;
+	}
+}
+
+export default {
+	install (Vue, options={}) {
+		Vue.prototype.$spinner = Spinner;
+	}
+}
---
+++ readme.md
@@ -0,0 +1,13 @@
+# 我给了几个默认的效果, 以后如果需要继续自己加就好
+
+# 添加方法
+1 新建一个 .vue 文件, 比如 SpinnerComponent.vue
+2 拷贝当前 .vue 内容到新建的文件里面
+3 修改 index.js 里面的 引入路径
+4 结束
+
+
+# 目前存在的一些效果
+SpinnerComponent.vue (default) 一个炫酷的效果
+	http://codepen.io/Mattykins/pen/wfJqk
+
